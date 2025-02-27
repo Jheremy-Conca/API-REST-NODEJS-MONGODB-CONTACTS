5b101cf92a4d3f46d158428457c0b1ed9958bf24
ConcaJheremy <concajheremy@gmail.com>
2025-02-27 14:44:30 -0500

    CORS

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const errorHandler = require("./middleware/errorHandler");
 const connectDB = require("./connection/dbConnection");
-
+const cors = require("cors");
 connectDB();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contacRoutes"));
 app.use(errorHandler);
